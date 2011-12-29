4cc2888185296564d1a6e6472ba17200ff042d80
JensDiemer <git@jensdiemer.de>
2011-12-29 21:05:52 +0100

    from http://www.mikrocontroller.net/topic/240238?goto=2477412#2477389

---
+++ sataboot/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+[ "x$1" == "x"] && {
+    echo "Usage: $0 hard-disk-device"
+    exit 1
+}
+
+dd if=/dev/zero of=$1 bs=512 count=65536
+dd if=signature.bin of=$1 bs=512 seek=57080
+dd if=stage1.bin of=$1 bs=512 seek=34
+dd if=stage1.bin of=$1 bs=512 seek=57088
+dd if=u-boot-1.1.2-sata.wrapped of=$1 seek=154 bs=512
+dd if=u-boot-1.1.2-sata.wrapped of=$1 seek=57208 bs=512
---
+++ sataboot/kernel-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+[ "x$1" == "x"] && {
+    echo "Usage: $0 hard-disk-device kernel"
+    exit 1
+}
+
+[ "x$2" == "x"] && {
+    echo "Usage: $0 hard-disk-device kernel"
+    exit 1
+}
+
+dd "if=$2" of=$1 seek=1290 bs=512
+dd "if=$2" of=$1 seek=58344 bs=512
