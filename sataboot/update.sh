beff4ed836fbbd362b1e73b8ba231b6f2a7bcad6
ft <sven@i7.(none)>
2011-12-30 00:31:37 +0100

    added update script for stage1 and u-boot

---
+++ sataboot/update.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+[ "x$1" == "x"] && {
+    echo "Usage: $0 hard-disk-device [[stage1-variant] u-boot-variant]"
+    echo "Stage1 Variants: 700 750"
+    echo "U-Boot Variants: 1.1.2-WarheadsSE 1.1.2-sata"
+    exit 1
+}
+
+stage1variant=750
+[ "x$2" != "x" ] && {
+    stage1variant="$2"
+}
+
+ubootvariant="1.1.2-WarheadsSE"
+[ "x$3" != "x" ] && {
+    ubootvariant="$3"
+}
+
+dd if=mbr.bin of=$1 bs=1 count=446
+dd if=stage1.wrapped$stage1variant of=$1 bs=512 seek=34
+dd if=stage1.wrapped$stage1variant of=$1 bs=512 seek=57088
+dd if=u-boot-$ubootvariant.wrapped of=$1 seek=154 bs=512
+dd if=u-boot-$ubootvariant.wrapped of=$1 seek=57208 bs=512
