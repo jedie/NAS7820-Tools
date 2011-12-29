6ee992ab3ab7a6db2eb13919b51a758048feaa2c
ft <sven@i7.(none)>
2011-12-29 22:30:19 +0100

    made install.sh variants capable

--- sataboot/install.sh
+++ sataboot/install.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
 [ "x$1" == "x"] && {
-    echo "Usage: $0 hard-disk-device"
+    echo "Usage: $0 hard-disk-device [stage1-variant] [u-boot-variant]"
+    echo "Stage1 Variants: 700 750"
+    echo "U-Boot Variants: 1.1.2-WarheadsSE 1.1.2-sata"
     exit 1
+}
+
+stage1variant=750
+[ "x$2" != "x" ] && {
+    stage1variant="$2"
+}
+
+ubootvariant="1.1.2-WarheadsSE"
+[ "x$3" != "x" ] && {
+    ubootvariant="$3"
 }
 
 dd if=/dev/zero of=$1 bs=512 count=65536
 dd if=mbr.bin of=$1 bs=512
-dd if=signature.bin of=$1 bs=512 seek=57080
-dd if=stage1.bin of=$1 bs=512 seek=34
-dd if=stage1.bin of=$1 bs=512 seek=57088
-dd if=u-boot-1.1.2-sata.wrapped of=$1 seek=154 bs=512
-dd if=u-boot-1.1.2-sata.wrapped of=$1 seek=57208 bs=512
+dd if=stage1.wrapped$stage1variant of=$1 bs=512 seek=34
+dd if=stage1.wrapped$stagevariant of=$1 bs=512 seek=57088
+dd if=u-boot-$ubootvariant.wrapped of=$1 seek=154 bs=512
+dd if=u-boot-$ubootvariant.wrapped of=$1 seek=57208 bs=512
