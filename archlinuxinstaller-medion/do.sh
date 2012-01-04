ef70255175a5bd1fce3bdabdced90e8aff9fe37d
ft <sven@i7.(none)>
2012-01-05 00:30:58 +0100

    corrected uhdcpc call

--- archlinuxinstaller-medion/do.sh
+++ archlinuxinstaller-medion/do.sh
@@ -3,7 +3,7 @@
 stage1variant=750
 disk=/dev/sda
 
-/sbin/dhcpc -i egiga0
+/sbin/udhcpc -i egiga0
 if [ $? == 0 ]; then
     echo "OK, get IP via DHCP"
 else
@@ -12,14 +12,13 @@ else
     exit 1
 fi
 
-dd if=/dev/zero of=$disk bs=512 count=65536
-dd if=mbr.bin of=$disk bs=512
-dd if=stage1.wrapped$stage1variant of=$disk bs=512 seek=34
-dd if=stage1.wrapped$stage1variant of=$disk bs=512 seek=57088
-dd if=u-boot.wrapped of=$disk bs=512 seek=154
-dd if=u-boot.wrapped of=$disk bs=512 seek=57208
-dd if=uImage.nopci of=$disk bs=512 bs=512 seek=1290
-dd if=uImage.nopci of=$disk bs=512 bs=512 seek=58344
+dd if=/dev/zero of=$disk bs=512 count=65536 || exit 0
+dd if=stage1.wrapped$stage1variant of=$disk bs=512 seek=34 || exit 0
+dd if=stage1.wrapped$stage1variant of=$disk bs=512 seek=57088 || exit 0
+dd if=u-boot.wrapped of=$disk bs=512 seek=154 || exit 0
+dd if=u-boot.wrapped of=$disk bs=512 seek=57208 || exit 0
+dd if=uImage.nopci of=$disk bs=512 bs=512 seek=1290 || exit 0
+dd if=uImage.nopci of=$disk bs=512 bs=512 seek=58344 || exit 0
 
 ./parted <<EOF
 mklabel gpt
@@ -28,19 +27,19 @@ mkpart primary ext3 1114112s -1s
 quit
 EOF
 # Restore special MBR of PLX
-dd if=mbr.bin of=$disk count=446 bs=1
-mke2fs -j "$disk"2
-mkdir /install
-mount -t ext3 "$disk"2 /install
+mke2fs -j "$disk"2 || exit 0
+mkdir /install || exit 0
+mount -t ext3 "$disk"2 /install || exit 0
 cd /install
 if [ ! -f /mnt/parnerkey/ArchLinuxARM-oxnas-latest.tar.gz ] ; then
-    wget http://archlinuxarm.org/os/ArchLinuxARM-oxnas-latest.tar.gz
-    tar xfvz ArchLinuxARM-oxnas-latest.tar.gz
-    rm ArchLinuxARM-oxnas-latest.tar.gz
+    wget http://archlinuxarm.org/os/ArchLinuxARM-oxnas-latest.tar.gz || exit 0
+    tar xfvz ArchLinuxARM-oxnas-latest.tar.gz || exit 0
+    rm ArchLinuxARM-oxnas-latest.tar.gz || exit 0
 else
-    tar xfvz /mnt/parnerkey/ArchLinuxARM-oxnas-latest.tar.gz
+    tar xfvz /mnt/parnerkey/ArchLinuxARM-oxnas-latest.tar.gz || exit 0
 fi
 /sbin/ifconfig egiga0 | grep ether | awk "{ print \$2 }" >usr/local/mac_addr
-umount "$disk"2
+umount "$disk"2 || exit 0
+dd if=mbr.bin of=$disk count=446 bs=1
 
 reboot
